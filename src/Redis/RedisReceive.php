50fe686153ef1bf026d2991eae8eda5fff543890
Dominik Zogg <dominik.zogg@gmail.com>
2016-09-18 15:54:48 +0200

    use the abstract message

--- src/Redis/RedisReceive.php
+++ src/Redis/RedisReceive.php
@@ -60,19 +60,4 @@ final class RedisReceive extends AbstractMessageReceive
             );
         }
     }
-
-    /**
-     * @return array
-     *
-     * @throws MessageReceiveException
-     */
-    public function receiveAll(): array
-    {
-        $messages = [];
-        while (null !== $message = $this->receive()) {
-            $messages[] = $message;
-        }
-
-        return $messages;
-    }
 }
